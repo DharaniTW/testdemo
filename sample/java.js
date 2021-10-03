86ac5f8acec1415cbbca2483e3c495eed6cfd55e
DharaniTW <dharaniselvam05@gmail.com>
2021-10-03 16:37:32 +0530

    First commit

---
+++ sample/java.js
@@ -0,0 +1,9 @@
+var arr1 = [3,'a','a','a',2,3,'a',3,'a',2,4,9,3];
+function(array){
+    var count = {};
+    var compare = 0; 
+    var mostFrequent;
+    for(i=0,length=arr1.length;i++){
+
+    }
+}
---
+++ scripts.js
@@ -0,0 +1,74 @@
+// var  request= new XMLHttpRequest();
+// request.open('GET','https://restcountries.com/v3/all',true);
+// request.send();
+// request.onload=function(){
+// var result=JSON.parse(request.response);
+//     console.log(result);
+//     for(var i=0;i<result.length;i++){
+//       try{
+//         var name=result[i].name;
+//         var latlon=result[i].latlng;
+//         if(latlon.length === 0){
+//             throw new Error("invalid coordinates for the countries");
+//            }
+//            weatherdata(name,...latlon);
+//      }catch(e){
+//         console.log("invalid country"+name+e.message);
+//     }
+//   }
+// }
+
+// function weatherdata(name,lat,lang){
+//     var url=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lang}&appid=551d6e9a2318bc032c165bb852d79099&units=imperial`;
+//     var request=new XMLHttpRequest();
+//     request.open('GET',url,true);
+//     request.send();
+//     request.onload=function(){
+//         var data = JSON.parse(request.response);
+//         console.log(`${name}:${data.main.temp}`);
+//     }
+
+// }
+
+
+//step 01 :create XHR object/constructor
+var req=new XMLHttpRequest();
+//step 02:open a connection 
+req.open('GET','https://restcountries.com/v3/all',true);
+//step 03:initiate a connection
+req.send();
+//step 04:once the data loaded from the server succeffully
+req.onload=function(){
+    //here we are converting 
+var result=JSON.parse(req.response);
+    console.log(result);
+    for(var i=0;i<result.length;i++){
+        try{
+            var name=result[i].name;
+            var latlon=result[i].latlng;
+            if(latlon.length === 0) throw new Error("invalid coordinates for the country");
+               
+            
+            weatherdata(name,...latlon);
+        }catch(e){
+            console.log("invalid country"+name+e.message);
+        }
+     
+    //    console.log(`${name}:${latlon}`);
+    }
+}
+
+function weatherdata(name,lat,lang){
+    //lat->33
+    //long->65
+// console.log(`${name}:${lat}:${lang}`);
+var url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lang}&appid=b6ccb5ec692c5ad80b29c5c862060b8d&units=imperial`;
+var req=new XMLHttpRequest();
+req.open('GET',url,true);
+req.send();
+req.onload=function(){
+    var data=JSON.parse(req.response);
+    console.log(`${name}:${data.main.temp}`);
+}
+
+}
